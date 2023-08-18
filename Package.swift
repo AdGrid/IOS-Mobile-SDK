41726a5575b174d14ce600e66d3dc33b74a0e284
Vitalii Overchuk <voverchuk@mint-innovations.com>
2023-08-18 11:00:53 +0300

    add first version of library

--- Package.swift
+++ Package.swift
@@ -4,25 +4,62 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdGridIOSMobileSDKRepo",
+    name: "AdGridSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AdGridIOSMobileSDKRepo",
-            targets: ["AdGridIOSMobileSDKRepo"]),
+            name: "AdGridSDK",
+            targets: ["AdGridSDK",
+                      "AppLovinSDK",
+                      "FBLPromises",
+                      "GoogleAppMeasurement",
+                      "GoogleAppMeasurementIdentitySupport",
+                      "GoogleInteractiveMediaAds",
+                      "GoogleMobileAds",
+                      "GoogleUtilities",
+                      "nanopb",
+                      "OMSDK-Static_Prebidorg",
+                      "UserMessagingPlatform",
+                      "XCPrebidMobile",
+                      "XCPrebidMobileAdMobAdapters",
+                      "XCPrebidMobileGAMEventHandlers",
+                      "XCPrebidMobileMAXAdapters",
+                     ]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AdGridIOSMobileSDKRepo",
-            dependencies: []),
-        .testTarget(
-            name: "AdGridIOSMobileSDKRepoTests",
-            dependencies: ["AdGridIOSMobileSDKRepo"]),
+        .binaryTarget(name: "AdGridSDK",
+                      path: "Frameworks/AdGridSDK.xcframework"),
+        .binaryTarget(name: "AppLovinSDK",
+                      path: "Frameworks/AppLovinSDK.xcframework"),
+        .binaryTarget(name: "FBLPromises",
+                      path: "Frameworks/FBLPromises.xcframework"),
+        .binaryTarget(name: "GoogleAppMeasurement",
+                      path: "Frameworks/GoogleAppMeasurement.xcframework"),
+        .binaryTarget(name: "GoogleAppMeasurementIdentitySupport",
+                      path: "Frameworks/GoogleAppMeasurementIdentitySupport.xcframework"),
+        .binaryTarget(name: "GoogleInteractiveMediaAds",
+                      path: "Frameworks/GoogleInteractiveMediaAds.xcframework"),
+        .binaryTarget(name: "GoogleMobileAds",
+                      path: "Frameworks/GoogleMobileAds.xcframework"),
+        .binaryTarget(name: "GoogleUtilities",
+                      path: "Frameworks/GoogleUtilities.xcframework"),
+        .binaryTarget(name: "nanopb",
+                      path: "Frameworks/nanopb.xcframework"),
+        .binaryTarget(name: "OMSDK-Static_Prebidorg",
+                      path: "Frameworks/OMSDK-Static_Prebidorg.xcframework"),
+        .binaryTarget(name: "UserMessagingPlatform",
+                      path: "Frameworks/UserMessagingPlatform.xcframework"),
+        .binaryTarget(name: "XCPrebidMobile",
+                      path: "Frameworks/XCPrebidMobile.xcframework"),
+        .binaryTarget(name: "XCPrebidMobileAdMobAdapters",
+                      path: "Frameworks/XCPrebidMobileAdMobAdapters.xcframework"),
+        .binaryTarget(name: "XCPrebidMobileGAMEventHandlers",
+                      path: "Frameworks/XCPrebidMobileGAMEventHandlers.xcframework"),
+        .binaryTarget(name: "XCPrebidMobileMAXAdapters",
+                      path: "Frameworks/XCPrebidMobileMAXAdapters.xcframework"),
     ]
 )
